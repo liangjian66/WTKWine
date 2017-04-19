5c06f485a0e4d9e4fe8e6aa9a096a02182473cbb
韩小胜 <hanxiaosheng@hanxiaoshengdeMacBook-Pro-2.local>
2017-04-19 20:53:35 +0800

    提交2

--- WTKWineNew/WTKWineNew/AppDelegate.m
+++ WTKWineNew/WTKWineNew/AppDelegate.m
@@ -60,6 +60,14 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    NSLog(@"fasfdsdadf");
+    NSLog(@"fasfdsdadf");
+    NSLog(@"fasfdsdadf");
+    NSLog(@"fasfdsdadf");NSLog(@"fasfdsdadf");
+    NSLog(@"fasfdsdadf");
+    NSLog(@"fasfdsdadf");
+    NSLog(@"fasfdsdadf");
+    
 }
 
 
